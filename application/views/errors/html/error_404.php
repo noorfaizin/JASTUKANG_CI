a9a22b0ded8e9ba34e4d5c17993eb91bd1400d86
Noor Faizin <62752695+noorfaizin@users.noreply.github.com>
2021-01-09 11:20:19 +0700

    views

---
+++ application/views/errors/html/error_404.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>404 Page Not Found</title>
+    <link rel="shortcut icon" href="http://localhost/jastukang_ci/assets/images/icon.png">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<!-- Custom fonts for this template-->
+	<link rel="stylesheet" href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css'>
+    <link rel="stylesheet" href="../assets/vendor/fontawesome-free/css/all.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+	<!-- Custom styles for this template-->
+	<link rel="stylesheet" href="http://localhost/jastukang_ci/assets/css/sb-admin-2.css">
+	
+</head>
+<body>
+	<!-- Begin Page Content -->
+	<div class="container-fluid">
+
+		<!-- 404 Error Text -->
+		<div class="text-center">
+			<div class="error mx-auto" data-text="404">404</div>
+			<p class="lead text-gray-800 mb-5">Page Not Found</p>
+			<p class="text-gray-500 mb-0">Mohon maaf, halaman yang anda cari tidak ada</p>
+			<a href="http://localhost/jastukang_ci/">&larr; Kembali ke beranda</a>
+		</div>
+
+	</div>
+	<!-- /.container-fluid -->
+</body>
+</html>
